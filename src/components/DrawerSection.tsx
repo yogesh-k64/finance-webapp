c246ccdb1131b93a22dfc1f87b170e8c1580e1d4
Yogesh <yogesh.k@iouring.com>
2025-05-30 16:14:38 +0530

    added left section text

--- src/components/DrawerSection.tsx
+++ src/components/DrawerSection.tsx
@@ -7,6 +7,15 @@ const DrawerSection = () => {
                 <span>Finance Manager</span>
                 <UnfoldMoreOutlinedIcon className='collapse-icon' />
             </div>
+            <div className='page-label' >
+                <span>Customers</span>
+            </div>
+            <div className='page-label' >
+                <span>Handouts</span>
+            </div>
+            <div className='page-label' >
+                <span>Collection</span>
+            </div>
         </div>
     )
 }

b6974c5c12928380c4dd164397e8df5314537af3
yogesh-k64 <yogesh.k6499@gmail.com>
2025-12-14 15:45:50 +0000

    added production alias in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,15 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://middleware-finance-app-6sga.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+      }
+    }
   }
 })
